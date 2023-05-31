ddd8ab143beb1f8ff066ab1d023192ac33eb6a95
rullbachtiar2207 <129921564+rullbachtiar2207@users.noreply.github.com>
2023-05-31 11:35:00 +0700

    Create ShoppingCart01.java

---
+++ ShoppingCart01.java
@@ -0,0 +1,9 @@
+public class ShoppingCart01 {
+	public static void main(String [] args) {
+		String custName = "Brando";
+		String itemDesc = "Shirt";
+		String socialMed = "Youtube";
+		String message = custName + " is a youtuber in " + socialMed + " and i want to buy his " + itemDesc;
+		System.out.println(message);
+	}
+}
