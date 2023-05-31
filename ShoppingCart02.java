c4be790f8f690bb251dedf71f4da787d2dea6460
rullbachtiar2207 <129921564+rullbachtiar2207@users.noreply.github.com>
2023-05-31 16:51:48 +0700

    Update ShoppingCart02.java

--- ShoppingCart02.java
+++ ShoppingCart02.java
@@ -1,14 +1,13 @@
-
 public class ShoppingCart02 {
-	public static void main(String [] args) {
-		 double price = 5.0;
-		    double tax = 0.25;
-		    int quantity = 3;
-		    double totalPrice = (price * quantity) * (1 + tax);
-		    String custName = "Rama";
-		    String itemDesc = "Jacket";
-		    String message = custName + " wants to purchase " + quantity + " " + itemDesc;
-		    System.out.println(message);
-		    System.out.println("Total cost with tax is: $" + totalPrice);
-	}
+    public static void main(String [] args) {
+        double price = 5.0;
+        double tax = 0.25;
+        int quantity = 3;
+        double totalPrice = (price * quantity) * (1 + tax);
+        String custName = "Bachtiar";
+        String itemDesc = "Jacket";
+        String message = custName + " wants to purchase " + quantity + " " + itemDesc;
+        System.out.println(message);
+        System.out.println("Total cost with tax is: $" + totalPrice);
+    }
 }
