8121fc11f973028064d90ec24f0469f63d3fd5d4
rullbachtiar2207 <129921564+rullbachtiar2207@users.noreply.github.com>
2023-05-31 11:34:08 +0700

    Create ShoppingCart02.java

---
+++ ShoppingCart02.java
@@ -0,0 +1,14 @@
+
+public class ShoppingCart02 {
+	public static void main(String [] args) {
+		 double price = 5.0;
+		    double tax = 0.25;
+		    int quantity = 3;
+		    double totalPrice = (price * quantity) * (1 + tax);
+		    String custName = "Rama";
+		    String itemDesc = "Jacket";
+		    String message = custName + " wants to purchase " + quantity + " " + itemDesc;
+		    System.out.println(message);
+		    System.out.println("Total cost with tax is: $" + totalPrice);
+	}
+}
